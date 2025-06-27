807aa0f89ce19ce800f94e5e4d401d49c97254b8
d4Rk <darkdevilx@gmx.net>
2025-06-27 20:16:12 +0200

    added tab to blink.cmp, typr settings update

--- dot_config/nvim/lua/plugins/typr.lua
+++ dot_config/nvim/lua/plugins/typr.lua
@@ -2,7 +2,15 @@ return {
   "nvzone/typr",
   dependencies = "nvzone/volt",
   opts = {
-    -- kblayout = "qwertz",
+    mode = "phrases", -- words, phrases
+    winlayout = "responsive",
+    kblayout = "qwerty",
+    wpm_goal = 100,
+    numbers = false,
+    symbols = false,
+    random = false,
+    phrases = nil, -- can be a table of strings
+    insert_on_start = false,
   },
   cmd = { "Typr", "TyprStats" },
 }
