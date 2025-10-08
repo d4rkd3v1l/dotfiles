f2090d30687aea8fcfabc3345576efde17084de4
d4Rk <darkdevilx@gmx.net>
2025-10-08 09:12:08 +0200

    changed scratch window size

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -7,6 +7,11 @@ return {
         relative = "editor",
         col = -1,
       },
+      scratch = {
+        width = 0.75,
+        height = 0.75,
+        zindex = 20,
+      }
     },
     image = {
       enabled = true,
