6eca759ac4802f163c96ba074feca1324ccec255
d4Rk <darkdevilx@gmx.net>
2026-01-17 15:55:02 +0100

    fixed markdown linting, some cleanup

--- dot_config/nvim/lua/plugins/nvim-lint.lua
+++ dot_config/nvim/lua/plugins/nvim-lint.lua
@@ -1,13 +1,10 @@
--- Check which linter is currently used for a buffer use:
--- :lua print(vim.inspect(require('lint').linters_by_ft[vim.bo.filetype]))
-
 return {
   "mfussenegger/nvim-lint",
   optional = true,
   opts = {
     linters = {
-      markdownlint = {
-        args = { "--config", os.getenv("HOME") .. "/.config/nvim/.markdownlint.yaml", "--" },
+      ["markdownlint-cli2"] = {
+        prepend_args = { "--config", os.getenv("HOME") .. "/.config/nvim/.markdownlint.yaml", "--" },
       },
     },
   },
