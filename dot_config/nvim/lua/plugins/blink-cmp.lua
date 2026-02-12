5b3a4a7e80ccab3e0e67fe45e7da3983aef2a64a
d4Rk <darkdevilx@gmx.net>
2026-02-12 17:14:18 +0100

    make ctrl+j/k work in command line as well (cycle completions)

--- dot_config/nvim/lua/plugins/blink-cmp.lua
+++ dot_config/nvim/lua/plugins/blink-cmp.lua
@@ -1,12 +1,23 @@
 return {
   "saghen/blink.cmp",
-  opts = {
-    keymap = {
-      preset = "enter",
+  opts = function(_, opts)
+    -- Unified keymaps
+    local keymap = {
       ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
       ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
       ["<C-j>"] = { "select_next", "snippet_forward", "fallback" },
       ["<C-k>"] = { "select_prev", "snippet_backward", "fallback" },
-    },
-  }
+    }
+
+    -- Insert mode
+    opts.keymap = keymap
+    opts.keymap.preset = "enter"
+
+    -- Command mode
+    opts.cmdline = opts.cmdline or {}
+    opts.cmdline.enabled = true
+    opts.cmdline.keymap = keymap
+
+    return opts
+  end,
 }
